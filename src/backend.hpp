c94db9d997147e6021ef5b335b8e212ca117d15f
Jude Lingan <judecohenlingan@gmail.com>
2026-03-08 21:22:09 -0400

    added back deleted files

---
+++ src/backend.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <QObject>
+#include <QList>
+#include <QString>
+#include <qstandardpaths.h>
+#include "tasktree/tasktree.hpp"
+
+class TaskModel : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(tasktree::Task* ref READ getRef)
+    Q_PROPERTY(sqlite3_int64 id READ getId CONSTANT)
+    Q_PROPERTY(QString name READ getName NOTIFY nameChanged)
+
+public:
+    TaskModel(tasktree::Task* ref, QObject* parent = nullptr)
+        : QObject(parent), m_ref(ref), m_id(ref->get_id()), m_name(QString::fromStdString(ref->get_name())) {}
+
+	tasktree::Task* getRef() const noexcept { return m_ref; }
+    sqlite3_int64 getId() const noexcept { return m_id; }
+    QString getName() const noexcept { return m_name; }
+	
+	void setName(QString newName);
+
+signals:
+	void nameChanged(QString newName);
+
+private:
+	tasktree::Task* m_ref;
+    const sqlite3_int64 m_id;
+    QString m_name;
+};
+
+class Backend : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(QList<QObject*> tasks READ getTasks NOTIFY tasksChanged)
+
+public:
+    explicit Backend(QObject* parent = nullptr);
+    ~Backend();
+
+    QList<QObject*> getTasks() const { return m_tasks; }
+
+    Q_INVOKABLE void addTask(const QString& name);
+    Q_INVOKABLE void deleteTask(TaskModel* task);
+    Q_INVOKABLE void refreshTasks();
+
+signals:
+    void tasksChanged();
+
+private:
+    void loadTasks();
+
+    tasktree::TaskTree m_tree;
+    QList<QObject*> m_tasks;
+};
